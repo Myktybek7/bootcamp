da2bed290e74c673454bcefba9fda0fdab1a62ab
myktybekabdykaiymov@gmail.com <myktybekabdykaiymov@gmail.com>
2024-01-15 10:33:48 +0600

    lesson4(2.0)

--- demo/src/main/java/com/example/demo/controller/ProductController.java
+++ demo/src/main/java/com/example/demo/controller/ProductController.java
@@ -14,10 +14,7 @@ import java.util.List;
 public class ProductController {
     @Autowired
     private UserRepository userRepository;
-    @GetMapping("/product")
-    public String product(@RequestParam String name){
 
-    }
     @GetMapping("/hello")
     public String name(@RequestParam String name){
         return name+ " hello!";
